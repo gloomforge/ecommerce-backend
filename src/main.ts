add57004d129bf334942bd9cd10f56ffc7c3066f
gloomforge <softly2200@gmail.com>
2025-06-24 19:13:21 +0300

    feat: add ping module and update app configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const configService = app.get(ConfigService);
+  const port: number = configService.get<number>('APPLICATION_PORT') ?? 3000;
+
+  await app.listen(port).then(() => {
+    console.info(`App running on http://localhost:${port}`);
+  });
 }
 void bootstrap();
