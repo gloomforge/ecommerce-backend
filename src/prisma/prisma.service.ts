92835c9590138cedc833697a35f17c728c0b5ae5
gloomforge <softly2200@gmail.com>
2025-06-25 11:33:52 +0300

    feat: add prisma module and update app module

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,12 @@
+import { OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/__generated__';
+
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  public async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  public async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+}
