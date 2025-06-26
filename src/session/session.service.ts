7ebbb92a972a360f9d01d60d2411ea429db0dc0f
gloomforge <softly2200@gmail.com>
2025-06-26 09:39:44 +0300

    chore: update method in sessionService

--- src/session/session.service.ts
+++ src/session/session.service.ts
@@ -1,24 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@/prisma/prisma.service';
 import { ConfigService } from '@nestjs/config';
+import { randomBytes } from 'crypto';
 
 @Injectable()
 export class SessionService {
+  private ttl: number;
+
   public constructor(
     private readonly prismaService: PrismaService,
     private readonly configService: ConfigService,
-  ) {}
+  ) {
+    this.ttl = Number(this.configService.get<string>('SESSION_TTL'));
+  }
 
-  public async create(userId: number, userAgent?: string) {
-    const expireAt = new Date(
-      Date.now() + this.configService.getOrThrow<number>('SESSION_TTL'),
-    );
-    return await this.prismaService.session.create({
-      data: {
+  public async createSession(userId: number) {
+    await this.prismaService.session.deleteMany({
+      where: {
         userId,
-        expireAt,
-        userAgent,
       },
     });
+
+    const session = await this.prismaService.session.create({
+      data: {
+        userId,
+        token: randomBytes(32).toString('hex'),
+        expireAt: new Date(Date.now() + this.ttl),
+      },
+    });
+
+    return session;
   }
 }
