7a21965352b2fe17e599bc86b1cc8fb67aa8a4f6
gloomforge <softly2200@gmail.com>
2025-06-29 18:38:40 +0300

    chore: update user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,24 +1,57 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Patch } from '@nestjs/common';
 import { UserService } from './user.service';
 import { Authorization } from '@/auth/decorators/auth.decorator';
-import { UserRole } from '@prisma/__generated__';
-import { User } from '@prisma/__generated__';
+import { $Enums } from '@prisma/__generated__';
+import UserRole = $Enums.UserRole;
+import { Authorized } from '@/auth/decorators/authorized.decorator';
+import { UpdateUserDto } from './dto/update-user.dto';
 
+/**
+ * Контроллер для работы с пользователями
+ */
 @Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  /**
+   * Конструктор контроллера пользователей
+   * @param userService - сервис для работы с пользователями
+   */
+  public constructor(private readonly userService: UserService) {}
 
-  @Get('all')
-  @Authorization(UserRole.ADMIN)
-  public async findAll(): Promise<Omit<User, 'password'>[]> {
-    return await this.userService.findAll();
+  /**
+   * Получает профиль текущего пользователя
+   * @param userId - id авторизированного пользователя
+   * @returns профиль пользователя
+   */
+  @Authorization()
+  @HttpCode(HttpStatus.OK)
+  @Get('profile')
+  public async findProfile(@Authorized('id') userId: string) {
+    return this.userService.findById(userId);
   }
 
-  @Get('by-id/:id')
+  /**
+   * Обновляет профиль текущего пользователя
+   * @param userId - id авторизованного пользователя
+   * @param model - данные для обновления профиля
+   * @returns обновленный профиль пользователя
+   */
+  @Authorization()
+  @HttpCode(HttpStatus.OK)
+  @Patch('profile')
+  public async updateProfile(
+    @Authorized('id') userId: string,
+    @Body() model: UpdateUserDto,
+  ) {
+    return this.userService.update(userId, model);
+  }
+
+  /**
+   * Получение всех пользователей (доступ только для администраторов)
+   * @returns ответ от сервиса пользователей (возвращает массив пользователей без пароля)
+   */
+  @Get()
   @Authorization(UserRole.ADMIN)
-  public async findById(
-    @Param('id', ParseIntPipe) id: number,
-  ): Promise<Omit<User, 'password'>> {
-    return await this.userService.findById(id);
+  public async findAll() {
+    return this.userService.findAll();
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,86 +1,120 @@
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '@/prisma/prisma.service';
-import { Account, User } from '@prisma/__generated__';
+import { User } from '@prisma/__generated__';
+import { UpdateUserDto } from './dto/update-user.dto';
 
+/**
+ * Сервис для работы с пользователями
+ */
 @Injectable()
 export class UserService {
+  /**
+   * Конструктор сервиса пользователей
+   * @param prismaService - сервис для работы с базой данных
+   */
   public constructor(private readonly prismaService: PrismaService) {}
 
-  public async findAll(): Promise<Omit<User, 'password'>[]> {
-    const users: User[] = await this.prismaService.user.findMany();
+  /**
+   * Находит всех пользователей в бд и удаляет пароль
+   * @returns массив пользователей без пароля
+   */
+  public async findAll() {
+    const users = await this.prismaService.user.findMany();
+    const container: Omit<User, 'password'>[] = [];
 
-    return users.map((user) => {
-      const { password: _, ...otherData } = user;
-      return otherData;
+    users.map((user) => {
+      const { password: _, ...data } = user;
+      container.push(data);
     });
+
+    return container;
   }
 
-  public async findById(id: number): Promise<User> {
-    const user: User | null = await this.prismaService.user.findUnique({
+  /**
+   * Находит пользователя по ID
+   * @param id - ID пользователя
+   * @returns найденный пользователь
+   * @throws {NotFoundException} если пользователь не найден
+   */
+  public async findById(id: string) {
+    const user = await this.prismaService.user.findUnique({
       where: {
         id,
       },
+      include: {
+        account: true,
+      },
     });
 
     if (!user) {
       throw new NotFoundException(
-        'Пользователь не найден. Пожалуйста , проверьте введенные данные',
+        'Пользователь не найден. Пожалуйста, проверьте введенные данные.',
       );
     }
-    return user;
+
+    const { password: _, ...data } = user;
+    return data;
   }
 
-  public async findByEmail(email: string): Promise<User> {
-    const user: User | null = await this.prismaService.user.findUnique({
+  /**
+   * Находит пользователя по email
+   * @param email - email пользователя
+   * @returns {Promise<User | null>} найденный пользователь или null, если не найден
+   */
+  public async findByEmail(email: string): Promise<User | null> {
+    return this.prismaService.user.findUnique({
       where: {
         email,
       },
+      include: {
+        account: true,
+      },
     });
-
-    if (!user) {
-      throw new NotFoundException(
-        'Пользователь не найден. Пожалуйста , проверьте введенные данные',
-      );
-    }
-    return user;
   }
 
-  public async create(
-    fullName: string,
-    email: string,
-    password: string,
-  ): Promise<Omit<User, 'password'> & { account: Account }> {
-    const exists: User | null = await this.prismaService.user.findUnique({
-      where: { email },
+  /**
+   * Обновляет данные пользователя
+   * @param userId - id пользователя
+   * @param model - данные для обновления пользователя
+   * @returns обновленный пользователь
+   */
+  public async update(userId: string, model: UpdateUserDto) {
+    const user = await this.findById(userId);
+
+    return await this.prismaService.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        email: model.email,
+        account: {
+          update: {
+            fullName: model.name,
+          },
+        },
+      },
+      include: { account: true },
     });
+  }
 
-    if (exists) {
-      throw new ConflictException('Пользователь с таким email уже существует');
-    }
-
-    const newUser: User = await this.prismaService.user.create({
+  /**
+   * Создает нового пользователя
+   * @param email - email пользователя
+   * @param password - пароль пользователя
+   * @param displayName - отображаемое имя пользователя
+   * @returns созданный пользователь
+   */
+  public async create(email: string, password: string, displayName: string) {
+    return this.prismaService.user.create({
       data: {
         email,
         password,
-      },
-    });
-
-    const account: Account = await this.prismaService.account.create({
-      data: {
-        userId: newUser.id,
-        fullName,
-        constact: {
-          create: [
-            {
-              type: 'EMAIL',
-              value: email,
-            },
-          ],
+        account: {
+          create: {
+            fullName: displayName,
+          },
         },
       },
     });
-
-    const { password: _, ...otherData }: User = newUser;
-    return { ...otherData, account };
   }
 }
